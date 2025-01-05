e84446e8e86c7cba73748a28a4f979c1b1173173
Nicocoooooo <nicolasantoine2003@gmail.com>
2025-01-05 14:15:10 +0100

    feat: Ajout du layout principal avec header et sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from './components/layouts/MainLayout';
 import DashboardPage from './pages/dashboard/index';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-white">
-        <main className="p-8">
-          <Routes>
-            <Route path="/" element={<DashboardPage />} />
-          </Routes>
-        </main>
-      </div>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+        </Routes>
+      </MainLayout>
     </Router>
   );
 }
